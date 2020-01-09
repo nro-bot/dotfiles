f6aa59a4af7c9086197806d42ba0da06f7034bca
N Ouyang <nouyang@alum.mit.edu>
2020-01-09 17:04:27 -0500

    fix typo

--- scripts/programs/venv.sh
+++ scripts/programs/venv.sh
@@ -1,5 +1,5 @@
-" Install machine learning basics "
+# Install machine learning basics 
 python3 -m venv ~/v3 
-source ~/v3/env/activate
+source ~/v3/bin/activate
 pip3 install torch torchvision opencv-python 
 deactivate
