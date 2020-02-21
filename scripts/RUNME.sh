1e33058a0cf50bdb480e54ef2e76d3a74558a04a
N Ouyang <nouyang@alum.mit.edu>
2020-02-20 20:10:51 -0500

    gnome extnesions instructions

--- scripts/RUNME.sh
+++ scripts/RUNME.sh
@@ -12,6 +12,9 @@ sudo apt upgrade -y
 echo "Remember to add ssh key to github. And go to gedit>preferences>plugins to enable font rescaling"
 echo "Edit vimrc to use molokai colorscheme"
 echo "Edit gnome-termianl preferences > profile > unamed > built-in schemes > grey on black"
+echo "Now gnome extensions: enable Gtile (no changes), vitals (check load and cpu usage)"
+echo "dash to panel (panel top, taskbar left with fixed center plugin, clock right of plugin icons)"
+gnome-shell-extension-prefs
 source ~/.bashrc
 
 # Fun hello
