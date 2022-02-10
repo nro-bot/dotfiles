27c98d5b5d6d42924e13a56783b9634fc1d271bc
N Ouyang <nouyang@alum.mit.edu>
2022-02-10 12:02:16 -0500

    edited venv (not sure keep or not

--- scripts/program_settings/venv.sh
+++ scripts/program_settings/venv.sh
@@ -1,7 +1,14 @@
 # Install machine learning basics 
 python3 -m venv ~/v3 
 source ~/v3/bin/activate
+pip install --upgrade pip
+
 pip3 install wheel
-pip3 install torch torchvision opencv-python 
 pip3 install black pylint flake8 autopep8 jedi
+
+pip3 install numpy scipy matplotlib jupyterlab pandas seaborn && deactivate
+pip3 install pillow 
+pip3 install torch torchvision opencv-python 
+# pip3 install keras tensorflow gym opencv-contrib-python 
+
 deactivate
