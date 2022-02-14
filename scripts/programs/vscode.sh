21a047be2e3ce975e762c51d466590c4252dbe43
N Ouyang <nouyang@alum.mit.edu>
2022-02-14 15:55:00 -0500

    added vscode settings; fixed bugs in cp vscode settings

--- scripts/programs/vscode.sh
+++ scripts/programs/vscode.sh
@@ -1,4 +1,6 @@
-echo "⌨️  Installing VSCode extensions"
+#!/bin/bash
+
+echo "📝 Installing VSCode extensions"
 
 function vsinstall {
   name="${1}"
@@ -14,3 +16,28 @@ vsinstall ms-vscode-remote.remote-ssh
 vsinstall ms-vscode-remote.remote-ssh-edit
 vsinstall VisualStudioExptTeam.vscodeintellicode
 vsinstall vscodevim.vim
+
+function linkSettingsfile {
+  dest="${HOME}/.config/Code/User/settings.json"
+  dateStr=$(date +%Y-%m-%d-%H%M)
+
+  if [ -h "${dest}" ]; then
+    # Existing symlink
+    echo "Removing existing file: ${dest}"
+    rm ${dest}
+
+  elif [ -f "${dest}" ]; then
+    # Existing file
+    echo "Backing up existing file: ${dest}"
+    mv ${dest}{,.${dateStr}}
+
+  fi
+
+  echo "Creating new symlink: ${dest}"
+  #ln -s ${pwd}/vscode_settings.json ${dest} 
+  cp ./vscode_settings.json ${dest} 
+}
+
+echo "🔗 Linking VS Code settings file"
+
+linkSettingsfile
