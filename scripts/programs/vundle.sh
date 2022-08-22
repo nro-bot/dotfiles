5d0618ca6cbe0518ec2a58896cc4f471be71456c
nouyang <nouyang@alum.mit.edu>
2022-08-22 16:36:46 -0400

    Update vundle.sh

--- scripts/programs/vundle.sh
+++ scripts/programs/vundle.sh
@@ -4,6 +4,6 @@ echo "📦 Installing Vundle"
 # Vundle plugin manager
 mkdir -p $HOME/.vim/bundle
 cd $HOME/.vim/bundle
-git clone git://github.com/VundleVim/Vundle.vim.git
+git clone https://github.com/VundleVim/Vundle.vim.git
 vim +PluginInstall +qall
 
